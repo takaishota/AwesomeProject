2ddbdac7fb4e5b244b6b90a1d3dfcd0cf86d2d81
高井翔太 <takaishota@gmail.com>
2018-03-28 13:03:06 +0900

    .

--- js/store/configureStore.js
+++ js/store/configureStore.js
@@ -1,5 +1,6 @@
 import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
+import logger from "redux-logger";
 import reducers from "../reducers/index";
 
-export default createStore(reducers, applyMiddleware(thunk));
+export default createStore(reducers, applyMiddleware(thunk, logger));
